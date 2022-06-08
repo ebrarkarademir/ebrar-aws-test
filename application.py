9bfa1c0e60b9ff5d2d7da8af9bc444016d791d04
ebrarcode <ebrarcode@hotmail.com>
2022-06-08 07:53:25 +0300

    first deployement change.

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello'
+    return 'Hi this is my first change on the deployed project.'
 
 if __name__ == '__main__':
     application.run()
